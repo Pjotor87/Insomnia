cda5cf2d5e2ff11d7b5470c07bcff49d1fc75a48
Andersson Peter <peter.andersson@tieto.com>
2019-07-13 09:44:56 +0200

    Refactoring

--- Insomnia/Utils.cs
+++ Insomnia/Utils.cs
@@ -1,4 +1,6 @@
-﻿namespace Insomnia
+﻿using System.Collections.Generic;
+
+namespace Insomnia
 {
     public static class Utils
     {
@@ -14,6 +16,41 @@
                 if (!IsNumeric(strArr[i]))
                     return false;
             return true;
+        }
+
+        public static IList<KeyValuePair<string, string>> ParseCommandlineArgs(string[] args)
+        {
+            return ParseCommandlineArgs(args, "-");
+        }
+
+        public static IList<KeyValuePair<string, string>> ParseCommandlineArgs(string[] args, string argKeyStartsWith)
+        {
+            IList<KeyValuePair<string, string>> parsedCommandlineArgs = new List<KeyValuePair<string, string>>();
+
+            if (args != null && args.Length >= 1)
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string argumentKey = 
+                        args[i].StartsWith(argKeyStartsWith) ? 
+                        args[i].Remove(0, argKeyStartsWith.Length).ToLower() : 
+                        string.Empty;
+
+                    if (string.IsNullOrEmpty(argumentKey) ||
+                        args.Length == i)
+                        continue;
+                    else
+                    {
+                        string argumentValue = 
+                            (args.Length >= (i + 1)) ? 
+                            args[i + 1] : 
+                            string.Empty;
+
+                        if (!string.IsNullOrEmpty(argumentValue))
+                            parsedCommandlineArgs.Add(new KeyValuePair<string, string>(argumentKey, argumentValue));
+                    }
+                }
+
+            return parsedCommandlineArgs;
         }
     }
 }
